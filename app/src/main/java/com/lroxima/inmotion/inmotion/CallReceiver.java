05a1dfbcd0e458c424bc5e3a3d51d50ed8898841
leondacosta <leon@dacosta.me>
2017-04-22 11:59:03 +0100

    Created classes to handle Audio and Calls

---
+++ app/src/main/java/com/lroxima/inmotion/inmotion/CallReceiver.java
@@ -0,0 +1,49 @@
+package com.lroxima.inmotion.inmotion;
+
+import android.content.Context;
+
+import java.util.Date;
+
+/**
+ * Created by leon on 22/04/17.
+ */
+
+public class CallReceiver extends PhonecallReceiver {
+
+    @Override
+    protected void onIncomingCallReceived(Context ctx, String number, Date start)
+    {
+        //
+    }
+
+    @Override
+    protected void onIncomingCallAnswered(Context ctx, String number, Date start)
+    {
+        //
+    }
+
+    @Override
+    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end)
+    {
+        //
+    }
+
+    @Override
+    protected void onOutgoingCallStarted(Context ctx, String number, Date start)
+    {
+        //
+    }
+
+    @Override
+    protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end)
+    {
+        //
+    }
+
+    @Override
+    protected void onMissedCall(Context ctx, String number, Date start)
+    {
+        //
+    }
+
+}
